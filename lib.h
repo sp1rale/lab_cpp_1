5b16d0e447e00e7fe2aaae5de284527c5b7fe84a
sp1rale <dimayaremchyk24@gmail.com>
2023-11-25 18:54:48 +0200

    Add project files.

---
+++ lib.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define print(val) cout << val 
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define SQUARE(x) ((x) * (x))
+#define IS_EVEN(number) ((number) % 2 == 0)
---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include "lib.h"
+
+
+//ex1
+//int main() {
+	//int a = 14;
+	//int b = 88;
+
+	//int min = MIN(a, b);
+
+	//print ("Min num is: ") << min << endl;
+//}
+
+//ex2
+//int main() {
+	//int a = 14;
+	//int b = 88;
+
+	//int max = MAX(a, b);
+
+	//print("Max num is: ") << max << endl;
+	// 
+	// return 0;
+//}
+
+//ex3
+//int main() {
+	//int number = 14;
+	
+	//int square = SQUARE(number);
+
+	//print("Square is: ") << square << endl;
+
+	//return 0;
+//}
+
+//ex4
+//int main() {
+	//int number1 = 14;
+	//int number2 = 88;
+
+	//if (IS_EVEN(number1)) {
+		//print(number1) << " is even" << endl;
+	//}
+	//else {
+		//print(number1) << " is odd" << endl;
+	//}
+
+	//if (IS_EVEN(number2)) {
+		//print(number2) << " is even" << endl;
+	//}
+	//else {
+		//print(number2) << " is odd" << endl;
+	//}
+
+	//return 0;
+//}
+
+//ex5
+
+//int main() {
+//	int number1 = 23;
+//	int number2 = 823;
+//
+//	*if (IS_EVEN(number1)) {
+//		print(number1) << " is even" << endl;
+//	}
+//	else {
+//		print(number1) << " is odd" << endl;
+//	}
+//
+//	if (IS_EVEN(number2)) {*/
+//		print(number2) << " is even" << endl;
+//	}
+//	else {
+//		print(number2) << " is odd" << endl;
+//	}
+//
+//	return 0;
+//}
+
+
